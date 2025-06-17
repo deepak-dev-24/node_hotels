839e20b0a64e0841b9416a766f5efafb5f4cf891
Deepak Kumar <dk.yadav.dev.24.@gmail.com>
2025-06-17 08:25:04 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express(); 
 const db = require('./db'); 
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-
-const Menu = require('./models/Menu');
+const PORT = process.env.PORT | 3000;
 
 app.get('/', (req, res) => {
   res.send('welcome to my learning ')
@@ -17,6 +17,6 @@ const menuRoutes = require('./routs/menuRouts');
 app.use('/person',personsRouts);
 app.use('/menu',menuRoutes);
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("\n\nlistening on port number 3000");
 })
